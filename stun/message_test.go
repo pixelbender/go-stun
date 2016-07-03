e28133812e422607895b76ab3da6ab5dabca5a45
pixelbender <pixelbender@yandex.ru>
2016-07-03 20:31:02 +0300

    Add comments

---
+++ stun/message_test.go
@@ -0,0 +1,26 @@
+package stun
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestMessageDecode(t *testing.T) {
+	cases := []string{
+		"000100002112a4425179754d624b4f71642f6d6f",
+	}
+	dec := new(Decoder)
+	for _, it := range cases {
+		b, err := hex.DecodeString(it)
+		if err != nil {
+			t.Fatalf("decode error %v", err)
+		}
+		m, err := dec.ReadMessage(b)
+		if err != nil {
+			t.Fatalf("read error %v", err)
+		}
+		if m == nil {
+			t.Fatalf("read error")
+		}
+	}
+}
